1717938b1a92da3a863c6f129dc62630a069ef25
aazevedo-ti <andre.w.aze@hotmail.com>
2026-03-25 08:54:31 -0300

    Adicionando comando de busca de arquivos

--- monitoramento-logs.sh
+++ monitoramento-logs.sh
@@ -3,3 +3,7 @@
 LOG_DIR="../myapp/logs"
 
 echo "Verificando logs no diretorio $LOG_DIR"
+
+find $LOG_DIR -name "*.log" -print0 | while IFS= read -r -d '' arquivo; do
+        echo "Arquivo encontrado $arquivo"
+done
